353b6b20c141803e05a765085089e9a57d115ba6
mathebes <shimane.mathebe@vcontractor.co.za>
2021-11-30 19:18:54 +0200

    initial commit

---
+++ src/test/java/com/za/assessment/coindispenser/coindispenser/CoindispenserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.za.assessment.coindispenser.coindispenser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoindispenserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
